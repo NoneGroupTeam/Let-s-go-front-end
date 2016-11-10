d1e434d98ba3fb5de2e88f4d7e5766c29e60ac82
lc4t <lc4t0.0@gmail.com>
2016-11-10 23:07:59 +0800

    /login

---
+++ src/components/navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="navbar">
+    <nav class="navbar">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <!-- <div class="navbar-header"> -->
+
+        <!-- </div> -->
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <router-link to="/"><img id="logo-with-font" src="../assets/img/logo-with-font.png" class="logo"></router-link>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+      };
+    },
+  };
+</script>
+
+<style scoped>
+body
+{
+  background-color: #fff;
+}
+.logo
+{
+  max-width: 100%;
+  height:auto;
+}
+</style>
